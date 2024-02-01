76172fa9f973207de4c4fcd18934c6a659564227
CCTEVP <cce.evalencia@gmail.com>
2024-02-01 13:11:27 +0100

    Added QueryCampaign endpoint

--- Zync.Api/Controllers/QueryController.cs
+++ Zync.Api/Controllers/QueryController.cs
@@ -12,11 +12,26 @@ namespace Zync.API.Controllers
         public IActionResult queryCreative([FromQuery] string creativeId)
         {
             Response.ContentType = "application/json";
-
             MongoConnection connection = new MongoConnection("Spain");
-            Creative document = connection.filterCreativeById(creativeId);//
+            Creative document = connection.filterCreativeById(creativeId);
+            return Ok(document);
+        }
+
+
+    }
+    [Route("api/[controller]")]
+    public class QueryCampaignController : ControllerBase
+    {
+
+        [HttpGet(Name = "queryCampaign")]
+        public IActionResult queryCampaign([FromQuery] string campaignId)
+        {
+            Response.ContentType = "application/json";
+            MongoConnection connection = new MongoConnection("Spain");
+            Campaign document = connection.filterCampaignById(campaignId);
             return Ok(document);
         }
 
     }
+
 }
--- Zync.Api/Models/Output/Campaign.cs
+++ Zync.Api/Models/Output/Campaign.cs
@@ -2,5 +2,14 @@
 {
     public class Campaign
     {
+        public string ID { get; set; }
+        public string CreatedAt { get; set; }
+        public string UpdatedAt { get; set; }
+        public string Name { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+        public string Advertiser { get; set; }
+        public int Week { get; set; }
+        public string OrderAida { get; set; }
     }
 }
